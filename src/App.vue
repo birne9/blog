2faa3422d0cfe3c48cb09fd8331776d0b4fa0e7e
zhanjiamin <17317296268@163.com>
2024-01-16 16:32:25 +0800

    test:测试

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <template>
     <router-view></router-view>
 </template>
-<script setup lang="ts">
-import { ref} from 'vue';
+<script setup>
 
-const isMounted:Boolean = ref(false);
 
 </script>
 <style lang="less" scoped>
