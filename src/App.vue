1308050ceba4971e7fa0ad80e28871f1fff21515
zhanjiamin <17317296268@163.com>
2024-01-16 16:04:57 +0800

    test:移动端还是pc端

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
     <router-view></router-view>
 </template>
-<script setup></script>
+<script setup lang="ts">
+import { ref, onMounted} from 'vue';
+import {isMobileDevice} from './utils/index'
+const isMounted:Boolean = ref(false);
+onMounted(()=>{
+    console.log(isMobileDevice() ? '手机端' : '电脑端')
+})
+</script>
 <style lang="less" scoped>
 
 </style>
