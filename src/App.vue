be607ea98897c8849928936460197ef4dae56c4a
zhanjiamin <17317296268@163.com>
2024-01-16 18:06:15 +0800

    test:调试引入问题

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,10 @@
     <router-view></router-view>
 </template>
 <script setup lang="ts">
-import {isMobileDevice} from './utils/index'
 import {onMounted } from 'vue'
 
 onMounted(()=>{
-    console.log(isMobileDevice())
+    console.log('我是binre9')
 })
 </script>
 <style lang="less" scoped>
