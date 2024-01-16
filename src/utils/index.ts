9bd5bd20aabc40fd3419558081c98718891b9789
zhanjiamin <17317296268@163.com>
2024-01-16 16:50:16 +0800

    test:测试代码

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,21 +1,22 @@
 // 判断是否为移动端
 export function isMobileDevice() {
-  const userAgentInfo = navigator.userAgent;
-  const mobileAgents = [
-    'Android',
-    'iPhone',
-    'SymbianOS',
-    'Windows Phone',
-    'iPad',
-    'iPod',
-  ];
-  let flag = false;
-  for (let v = 0; v < mobileAgents.length; v++) {
-    if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
-      flag = true;
-      break;
-    }
-  }
-  return flag;
+//   const userAgentInfo = navigator.userAgent;
+//   const mobileAgents = [
+//     'Android',
+//     'iPhone',
+//     'SymbianOS',
+//     'Windows Phone',
+//     'iPad',
+//     'iPod',
+//   ];
+//   let flag = false;
+//   for (let v = 0; v < mobileAgents.length; v++) {
+//     if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
+//       flag = true;
+//       break;
+//     }
+//   }
+//   return flag;
+return 'xxx'
 }
 
