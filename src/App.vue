c6f7ec2d0515e33c154623472b036584cfa3aef5
zhanjiamin <17317296268@163.com>
2024-01-16 18:12:55 +0800

    test:测试引入

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
     <router-view></router-view>
 </template>
 <script setup lang="ts">
-
+import {onMounted} from 'vue'
+onMounted(()=>{
+    console.log('我是birne9')
+})
 </script>
 <style lang="less" scoped>
 
