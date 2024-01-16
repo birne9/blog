6fdf90c704c49f817e0db0dd90a2fb74c3c9e380
zhanjiamin <17317296268@163.com>
2024-01-16 16:55:28 +0800

    test:调试代码

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,25 @@
 </template>
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import { isMobileDevice } from './utils/index'
+const isMobileDevice=()=>{ 
+  const userAgentInfo = navigator.userAgent;
+  const mobileAgents = [
+    'Android',
+    'iPhone',
+    'SymbianOS',
+    'Windows Phone',
+    'iPad',
+    'iPod',
+  ];
+  let flag = false;
+  for (let v = 0; v < mobileAgents.length; v++) {
+    if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
+      flag = true;
+      break;
+    }
+  }
+  return flag;
+}
 onMounted(()=>{
     console.log(isMobileDevice())
 })
