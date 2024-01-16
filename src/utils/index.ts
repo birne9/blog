7ea006187fd03f8b3944bc0bc0f269fd4c76a9b2
zhanjiamin <17317296268@163.com>
2024-01-16 16:26:15 +0800

    test:删除ts文件

--- src/utils/index.ts
+++
@@ -1,8 +0,0 @@
-
-//判断是否为移动端
-export function isMobileDevice() {
-    const isMobile = navigator.userAgent.match(
-        /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
-    ); // 是否移动端
-    return isMobile;
-}
