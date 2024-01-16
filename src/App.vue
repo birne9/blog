9c73489b8849bfb86fa72f95746598e339999753
zhanjiamin <17317296268@163.com>
2024-01-16 18:01:36 +0800

    test:调试

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,12 @@
     <router-view></router-view>
 </template>
 <script setup lang="ts">
+import {isMobileDevice} from './utils/index'
+import {onMounted } from 'vue'
 
-
+onMounted(()=>{
+    console.log(isMobileDevice())
+})
 </script>
 <style lang="less" scoped>
 
---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+// 判断是否是移动端
+function isMobileDevice() {
+  return '我是birne9'
+}
