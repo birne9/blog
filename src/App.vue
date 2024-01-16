34b37708a95913949b83e108a4f879e5df046d8a
zhanjiamin <17317296268@163.com>
2024-01-16 16:57:28 +0800

    test:删除代码

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,7 @@
     <router-view></router-view>
 </template>
 <script setup lang="ts">
-import { onMounted } from 'vue'
-const isMobileDevice=()=>{ 
-  const userAgentInfo = navigator.userAgent;
-  const mobileAgents = [
-    'Android',
-    'iPhone',
-    'SymbianOS',
-    'Windows Phone',
-    'iPad',
-    'iPod',
-  ];
-  let flag = false;
-  for (let v = 0; v < mobileAgents.length; v++) {
-    if (userAgentInfo.indexOf(mobileAgents[v]) > 0) {
-      flag = true;
-      break;
-    }
-  }
-  return flag;
-}
-onMounted(()=>{
-    console.log(isMobileDevice())
-})
+
 
 </script>
 <style lang="less" scoped>
