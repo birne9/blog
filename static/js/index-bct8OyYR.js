902d5cdbca263e2e1f442d701e82e0cc1b24e081
birne9 <108448045+birne9@users.noreply.github.com>
2024-01-16 08:58:05 +0000

    Deploying to gh-pages from  @ 34b37708a95913949b83e108a4f879e5df046d8a 🚀

--- static/js/index-bct8OyYR.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./_plugin-vue_export-helper-x3n3nnut.js";import{o as e,c as t}from"./index-YHwkmp-b.js";const c={},r={class:"about"};function s(n,a){return e(),t("div",r," about ")}const i=o(c,[["render",s]]);export{i as default};
