80a3f4a7530dc9dedcf70d3cafda3dbad7208fd2
zhanjiamin <17317296268@163.com>
2024-01-16 15:46:03 +0800

    test:测试

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,45 +1,19 @@
 <template>
-  <div class="home">
-    <div class="home_one">
-      <div class="home_avatar">
-        <img
-          src="../../static/images/avatar.jpeg"
-          alt="birne9"
-          title="birne9"
-        />
-      </div>
-      <div class="home_desc">🌈 We are who we choose to be.</div>
-    </div>
+  <div>
+    <home_h5 v-if='isMobile'></home_h5>
+    <home_pc v-else></home_pc>
   </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import home_h5 from './home_h5.vue';
+import home_pc from './home_pc.vue';
+import { ref } from 'vue';
+
+
+const isMobile:Boolean=ref(true)
+
+
+</script>
 <style lang="less" scoped>
-.home {
-  background-image: url("../../static/images/bigsea.jpeg");
-  width: 100vw;
-  height: calc(100vh - 144px);
-  background-position: center center;
-  object-fit: cover;
-  // background-repeat: no-repeat;
-  background-size: 100% 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  .home_avatar {
-    cursor: pointer;
-    display: flex;
-    justify-content: center;
-    img {
-      width: 100px;
-      height: 100px;
-    }
-  }
-  .home_desc {
-    text-align: center;
-    color: #fff;
-    font-size: 32px;
-    font-weight: 600;
-    margin-top: 10px;
-  }
-}
+
 </style>
