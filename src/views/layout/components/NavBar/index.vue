7e6c685689d5f11d1bdda0d9585035733a935b50
zhanjiamin <17317296268@163.com>
2024-01-16 15:54:11 +0800

    test:导航栏

---
+++ src/views/layout/components/NavBar/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <navbar_h5 v-if="isMobile"></navbar_h5>
+        <navbar_pc v-else></navbar_pc>
+    </div>
+</template>
+<script setup lang="ts">
+import navbar_h5 from './navbar_h5.vue';
+import navbar_pc from './navbar_pc.vue';
+import {  ref } from 'vue'
+
+// 获取设备store
+
+const isMobile:Boolean=ref(true)
+</script>
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/layout/components/NavBar/navbar_h5.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+       <div class="navbar_h5">
+            <div class="navbar_h5_left">
+                <div class="navbar_h5_left_img" @click="router.push('/')"><img src="../../../../static/images/avatar.jpeg" alt=""></div>
+            </div>
+            <div class="navbar_h5_right">
+
+            </div>
+       </div>
+    </div>
+</template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+const router = useRouter()
+</script>
+<style lang="less" scoped>
+.navbar_h5{
+    width: 100vw;
+    height: 90px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;;
+    .navbar_h5_left{
+        margin-left: 20px;
+        .navbar_h5_left_img{
+            img{
+                width: 44px;
+                height: 44px;
+                display: block;
+            }
+        }
+        .navbar_h5_left_one{
+            font-weight: 500;
+            font-size: 18px;
+            color: #000;
+            margin-left: 30px;
+            cursor: pointer;
+        }
+    }
+}
+</style>
