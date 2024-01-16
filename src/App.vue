2e909eef1c2e165b267eadac3f9440863915c378
zhanjiamin <17317296268@163.com>
2024-01-16 16:23:33 +0800

    test:调试打包

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
 </template>
 <script setup lang="ts">
 import { ref, onMounted} from 'vue';
-import {isMobileDevice} from './utils/index'
+// import {isMobileDevice} from './utils/index'
 const isMounted:Boolean = ref(false);
 onMounted(()=>{
-    console.log(isMobileDevice() ? '手机端' : '电脑端')
+    // console.log(isMobileDevice() ? '手机端' : '电脑端')
 })
 </script>
 <style lang="less" scoped>
