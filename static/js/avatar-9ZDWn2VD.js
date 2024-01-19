29e972d415968ae56e34a43a41b9e6c5933c4740
birne9 <108448045+birne9@users.noreply.github.com>
2024-01-19 10:05:58 +0000

    Deploying to gh-pages from  @ d13ba54214ed5a91ac0df0b45c217c1a4c40646b 🚀

--- static/js/avatar-9ZDWn2VD.js
+++
@@ -1,1 +0,0 @@
-const e=""+new URL("../jpeg/avatar-_xDJk_o3.jpeg",import.meta.url).href;export{e as _};
---
+++ static/js/wx_avatar-v1KzXlMr.js
@@ -0,0 +1,1 @@
+const r=""+new URL("../jpg/wx_avatar-yGEcLuCQ.jpg",import.meta.url).href;export{r as _};
