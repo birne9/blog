b2e682a629e072428aafb25577d37e0690975160
birne9 <108448045+birne9@users.noreply.github.com>
2026-09-13 11:04:14 +0000

    Deploying to gh-pages from @ birne9/blog@732c3230574fe18a0a68fb7d5fa5c1c82d2b6da5 🚀

---
+++ static/js/index-Pe7A3DvG.js
@@ -0,0 +1,1 @@
+import{m as i}from"./index-Bjumtvf1.js";const o=i("device",{state:()=>({isMobile:!1}),getters:{isMobileGetter:e=>e.isMobile},actions:{setIsMobile(e){this.isMobile=e}}});export{o as u};
