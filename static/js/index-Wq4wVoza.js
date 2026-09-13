89f6820bd12227fb06427abd113c67ba7848735a
birne9 <108448045+birne9@users.noreply.github.com>
2026-09-13 11:55:20 +0000

    Deploying to gh-pages from @ birne9/blog@d434409a818ed91c42499ce3b45fe6fc7eeb2144 🚀

--- static/js/index-Wq4wVoza.js
+++
@@ -1,1 +0,0 @@
-import{m as i}from"./index-Bh1Gtsf3.js";const o=i("device",{state:()=>({isMobile:!1}),getters:{isMobileGetter:e=>e.isMobile},actions:{setIsMobile(e){this.isMobile=e}}});export{o as u};
